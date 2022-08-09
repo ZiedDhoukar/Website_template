1cce475325327d9ee21a4770aae5b201d0e91912
ZiedDhoukar <105924677+ZiedDhoukar@users.noreply.github.com>
2022-08-09 11:58:47 +0100

    Add files via upload

---
+++ Booking.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booking</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet"href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+  <section id="book">
+    <style>
+        #book {
+        background-image: url('img/body 7/Design.png');
+        background-repeat: no-repeat;
+        background-attachment:scroll;
+        background-size: 100% 100%;
+        }
+        @media (max-width : 820px) {
+        .home{
+        background-size: contain;
+
+        }
+        }
+        </style>
+    <div>
+        <h1>Book ur program</h1>
+        <p>Neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt. Dictum <br> fusce ut placerat orci nulla pellentesque dignissim enim sit. </p>
+    </div>
+        <form action="" method="POST">
+          <div >
+            <div id="program">
+              <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+              <label for="vehicle1"> 
+                  <img src="/img/body 2/1.png" alt="">
+              <article>
+                  <h2>Program 1</h2>
+                  <p>sint occaecat cupidatat non</p>
+              </article>              </label><br>
+              <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+              <label for="vehicle2">
+                    <img src="/img/body 2/2.png" alt="">
+                <article>
+                    <h2>Program 2</h2>
+                    <p>sint occaecat cupidatat non</p>
+                </article>
+              </label><br>
+              <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+              <label for="vehicle3">
+                    <img src="/img/body 2/3.png" alt="">
+                <article>
+                    <h2>Program 3</h2>
+                    <p>sint occaecat cupidatat non</p>
+                </article>
+              </label><br> 
+              </div>
+              <div>         
+            <div>
+                <input type="text" name="name" class="b8-text" placeholder="Your Name" required="" />
+            </div>
+            <div>
+                <input type="text" name="age" class="b8-text" placeholder="Age" required="" />
+            </div>
+            <div>
+              <input type="email" name="" placeholder="Email" class="b8-text" id="" required>
+            </div>
+            <div>
+                <textarea height="147px"  rows="4" name="message" class="b8-text" placeholder="Messages" required=""></textarea>
+            </div>
+            <div>
+                <button type="submit">BOOK</button>
+            </div>
+        </form>
+</section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yoga</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet"href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+    <!--Header-->
+    <header>
+            <div class="LOGO">
+                <img src="h-Logo.png" alt="">
+            </div>
+            <div>
+                <nav>
+
+                    <ul>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                        <li><a href="#training">Training</a></li>
+                        <li><a href="#event">Event</a></li>
+                        <li><a href="#shop">Shop</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="Booking.php"><button>Book</button></a></li>
+
+                    </ul>
+                </nav>
+            </div>
+    </header>
+
+    <main>
+        <!--b1-->
+        <section id="b1">
+            <style>            
+            #b1 {
+                background-image: url('Design-b1.png');
+                background-repeat: no-repeat;
+                background-attachment:scroll;
+                background-size: 100% 100%;
+              }</style>
+            <div>
+                <img src="img-b1.png" width="100%" alt="">
+            </div>
+            <div>
+                <article>
+                    <h1>Balance Your Body And Mind</h1>
+                    <p>Duis aute irure dolor in repregenrerit in voliptate velit esse cillum dolore eu fugiat nulla paroatur. Excepteur sint occarcat cupidatat</p>
+                    <button>JOIN US NOW</button>
+                </article>
+            </div>
+        </section>
+        <!----------b2--------------->
+        <section id="b2">
+            <style>
+                .about {
+                background-color: #e5e3e1;
+              }
+              @media (max-width : 820px) {
+                .about{
+                background-size: 50% 50%;
+                    
+                }
+               }
+                </style>            
+                    <div>
+                        <img src="img-b2.png" alt="" class="b2-img">
+                    </div>
+                <div>                    
+                    <article>
+                        <h1>Pregnant<br>Women Program</h1>
+                        <p>Duis aute irure dolor in repregenrerit in voliptate velit esse cillum dolore eu fugiat nulla paroatur. Excepteur sint occarcat cupidatat</p>      
+                        <button>JOIN US NOW</button>
+                    </article>
+                </div>
+        </section>
+        <!----------b3----------------->
+        <section id="b3">
+                <div>
+                    <div>
+                        <img src="1-b3.png" alt="">
+                    </div>
+                    <article>
+                        <h2>Program 1</h2>
+                        <p>sint occaecat cupidatat non</p>
+                    </article>
+                </div>
+                <div>
+                    <div>
+                        <img src="2-b3.png" alt="">
+                    </div>
+                    <article>
+                        <h2>Program 2</h2>
+                        <p>sint occaecat cupidatat non</p>
+                    </article>
+                </div>
+                <div>
+                    <div>
+                        <img src="3-b3.png" alt="">
+                    </div>
+                    <article>
+                        <h2>Program 3</h2>
+                        <p>sint occaecat cupidatat non</p>
+                    </article>
+                </div>
+        </section>
+        <!--------b4----------->
+        <section id="b4">
+            <style>            
+            #b4 {
+                background-image: url('Design-b4.png');
+                background-repeat: no-repeat;
+                background-attachment:scroll;
+                background-size: 100% 100%;
+              }</style>
+            <div>
+                <article>
+                    <h1>Yoga Breathing or Pranayama</h1>
+                    <p>Duis aute irure dolor in repregenrerit in voliptate velit esse cillum dolore eu fugiat nulla paroatur. Excepteur sint occarcat cupidatat</p>
+                    <button>JOIN US NOW</button>
+                </article>
+            </div>
+            <div>
+                <img src="img-n4.png" width="100%" height="100%" alt="">
+            </div>
+        </section>
+        <!--------------b5--------------->
+        <section style="background-color: #e3e1df ;" id="b5">
+            <div>
+                <img src="img-b5.png">
+            </div>
+            <div>                    
+                <article>
+                    <img src="Ds-b5.png" alt="">
+                    <h1>Join Now and <br> Get 50% Off</h1>
+                    <p>Duis aute irure dolor in repregenrerit in voliptate velit esse cillum dolore eu fugiat nulla paroatur. Excepteur sint occarcat cupidatat</p>      
+                    <button>JOIN US NOW</button>
+                </article>
+            </div>
+        </section>
+        <!-------------------b6-->
+        <section id="b6">
+            <style>
+                #b6 {
+                background-image: url('Design-b6.png');
+                background-repeat: no-repeat;
+                background-attachment:scroll;
+                background-size: 100% 100%;
+                }
+                @media (max-width : 820px) {
+                .home{
+                background-size: contain;
+
+                }
+                }
+                </style>
+            <div id="b6-titre">
+                <h1>Galery</h1>
+                <p>Neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt. Dictum <br> fusce ut placerat orci nulla pellentesque dignissim enim sit. </p>
+            </div>
+            <div  id="b6-box">
+                <div>
+                    <div>
+                        <img src="1-b6.png">
+                        <h2 style="margin: 3% ;">Facilis Gravida</h2>
+                        <p>A lacus vestibulum sed. Amet purus gravida quis blandit turpis.</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                            <i><img src="2-b6.png"></i>
+                            <h2 style="margin: 3% ;">Facilis Gravida</h2>
+                            <p>A lacus vestibulum sed. Amet purus gravida quis blandit turpis.</p>
+                        
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        
+                            <i> <img src="3-b6.png"></i>
+                            <h2 style="margin: 3% ;">Facilis Gravida</h2>
+                            <p>A lacus vestibulum sed. Amet purus gravida quis blandit turpis.</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!---------------b7-------------------->
+        <section id="b7">
+            <style>
+                #b7 {
+                background-color: #e5ccc6;
+                }
+                @media (max-width : 820px) {
+                .about{
+                background-size: 50% 50%;
+                    
+                }
+                }
+                </style>
+                <div>
+                <div >
+                    <iframe class="yt" width="600px" height="423px" src="https://www.youtube.com/embed/Eml2xnoLpYE" title="Full Body Yoga for Strength & Flexibility | 25 Minute At Home Mobility Routine" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div>
+                    <article>
+                        <h1>Yoga Training Video Showreel</h1>
+                        <p>Duis aute irure dolor in repregenrerit in voliptate velit esse cillum dolore eu fugiat nulla paroatur. Excepteur sint occarcat cupidatat</p>
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>  
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                    </article>  
+                </div>
+                    </div>
+        </section>
+        <!---------------------b8------------------->
+        <section id="b8">
+            <style>
+                #b8 {
+                height: 826px;
+                background-image: url('Design-b8.png');
+                background-repeat: no-repeat;
+                background-attachment:scroll;
+                background-size: 100% 100%;
+                }
+                @media (max-width : 820px) {
+                .home{
+                background-size: contain;
+
+                }
+                }
+                </style>
+            <div>
+                <h1>Get in Touch</h1>
+                <p>Neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt. Dictum <br> fusce ut placerat orci nulla pellentesque dignissim enim sit. </p>
+            </div>
+                <form action="" method="POST">
+                    <div>
+                        <input type="text" name="name" class="b8-text" placeholder="Your Name" required="" />
+                    </div>
+                    <div>
+                        <input type="text" name="name" class="b8-text" placeholder="Gender" required="" />
+                    </div>
+                    <div>
+                        <textarea height="147px"  rows="4" name="message" class="b8-text" placeholder="Messages" required=""></textarea>
+                    </div>
+                    <div>
+                        <button type="submit">SEND MESSAGE</button>
+                    </div>
+                </form>
+        </section>
+    </main> 
+        <!----------------footer-->
+        <footer class="footer">
+            <div class="container">
+                <div class="row-footer">
+                 <div class="footer-col" id="1">
+                     <h4>Your Company</h4>
+                     <ul >
+                         <li class="space1">
+                             <a href=""><li>Halumine street 25 <br>Jakarta, indonesia <br>1250</li></a>
+                         </li>
+                         <li class="space2">
+                             <a href="wwww.yourdomain.com">wwww.yourdomain.com</a>
+                         </li>
+                     </ul>
+                 </div>
+                 <div class="footer-col" id="2">
+                     <h4>Sitemap</h4>
+                     <ul>
+                         <li><a href="#">Home</a></li>
+                         <li><a href="#">About</a></li>
+                         <li><a href="#">Blog</a></li>
+                         <li><a href="#">Menu</a></li>
+                         <li><a href="#">Store</a></li>
+                         <li><a href="#">Contact</a></li>
+                     </ul>
+     
+                 </div>
+                 <div class="footer-col" id="3">
+                     <h4>Lorem Ipsul</h4>
+                     <ul>
+                         <li><a href="#">Neque</a></li>
+                         <li><a href="#">Suspendisse</a></li>
+                         <li><a href="#">Dictum</a></li>
+                         <li><a href="#">Porttitor</a></li>
+                         <li><a href="#">Tincidunt</a></li>
+                         <li><a href="#">Enim lobortis</a></li>
+                     </ul>
+                 </div>
+                 <div class="footer-col" id="4">
+                     <h4>Sit Amet</h4>
+                     <ul>
+                         <li><a href="#">Neque</a></li>
+                         <li><a href="#">Suspendisse</a></li>
+                         <li><a href="#">Dictum</a></li>
+                         <li><a href="#">Porttitor</a></li>
+                         <li><a href="#">Tincidunt</a></li>
+                         <li><a href="#">Enim lobortis</a></li>
+                     </ul>
+     
+                 </div>
+                    <div class="footer-col" id="5">
+                        <a href="#"><img src="F-Logo.png" alt="" class="footer-logo"></a>
+                     <ul>
+                         <li><a href="">Copyright @ 2020 Freepik <br>Company S.L. All rights reserved.</a></li>
+                     </ul>
+                        <div class="social-links">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>  
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                         </div>
+                    </div>
+                </div>
+            </div>
+       </footer>
+</body>
+<script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    
+    // Get the button that opens the modal
+    var btn = document.getElementById("MyBtn");
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+    </script>
+</html>
